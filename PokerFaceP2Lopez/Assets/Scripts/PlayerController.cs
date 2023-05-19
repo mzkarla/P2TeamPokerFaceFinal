fee37ee4e8ac0dca77e0c7cdc9c6a0bdc73ee299
mzkarla <714875@student.dusd.net>
2023-05-19 09:25:46 -0700

    added the background
    added background, and basic player gameplay

--- PokerFaceP2Lopez/Assets/Scripts/PlayerController.cs
+++ PokerFaceP2Lopez/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dog : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float UpForce = 200f;
     private Rigidbody2D rb2d;
@@ -16,7 +16,7 @@ public class Dog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb2d.AddForce(new Vector2(0, UpForce));
         }
