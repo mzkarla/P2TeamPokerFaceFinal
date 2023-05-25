0855442eee92fe7bb3f0a9ce4a6d6e57d3aa519c
jojo <joannek715967@gmail.com>
2023-05-25 10:00:01 -0700

    I am working on creating a start and gameover screen, while trying to figure out the repeating background.
    i also created a spawnmanager which spawn everytime you click the left button.

--- PokerFaceP2Lopez/Assets/Scripts/RepeatingBackground.cs
+++ PokerFaceP2Lopez/Assets/Scripts/RepeatingBackground.cs
@@ -6,12 +6,19 @@ public class RepeatingBackground : MonoBehaviour
 {
     private BoxCollider2D groundCollider;
     private float groundHorizontalLength;
+    private float repeatWidth;
+    private Vector2 startPos;
+
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         groundCollider = GetComponent<BoxCollider2D>();
         groundHorizontalLength = groundCollider.size.x;
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        
     }
 
     // Update is called once per frame
@@ -21,10 +28,24 @@ public class RepeatingBackground : MonoBehaviour
         {
             RepositionBackground();
         }
+        if(transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos; 
+        }
     }
     private void RepositionBackground()
     {
         Vector2 groundOffset = new Vector2(groundHorizontalLength * 2f, 0);
         transform.position = (Vector2)transform.position + groundOffset;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("GameOver");
+        }
     }
 }
