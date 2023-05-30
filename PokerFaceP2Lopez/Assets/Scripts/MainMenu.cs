57fa136901f05c1a863cf0589873a1d05851b807
mzkarla <714875@student.dusd.net>
2023-05-30 08:54:38 -0700

    Added a Main Menu Script
    Added the main menu script to the main menu button

--- PokerFaceP2Lopez/Assets/Scripts/MainMenu.cs
+++ PokerFaceP2Lopez/Assets/Scripts/MainMenu.cs
@@ -9,4 +9,16 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
