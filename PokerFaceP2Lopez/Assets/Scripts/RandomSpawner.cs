630beffe126240f6e4a1aa979cacdc4722598dc2
jojo <joannek715967@gmail.com>
2023-05-30 10:05:49 -0700

    tried fixing errors from the console
    said something like public doesn't exist but it has an issue with the class not the actual scripts.

--- PokerFaceP2Lopez/Assets/Scripts/RandomSpawner.cs
+++ PokerFaceP2Lopez/Assets/Scripts/RandomSpawner.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using
-
-public class RandomSpawner : MonoBehaviour
+using 
+    public class RandomSpawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] enemyPrefabs;
