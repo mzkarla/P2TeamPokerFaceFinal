e88c582f1669a7343536a58100df69c6cf87553a
jojo <joannek715967@gmail.com>
2023-05-31 13:49:27 -0700

    i fixed the compile errors
    But the health, audio, and jumping need to be added. the collectibles too. I don't understand why he isn't jumping i have it written in the script.

--- PokerFaceP2Lopez/Assets/Scripts/PlayerController.cs
+++ PokerFaceP2Lopez/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,68 @@ public class PlayerController : MonoBehaviour
 {
     
     private Rigidbody2D rb2d;
+
+    //to make the player know its top from bottom
     public Transform ceilingCheck;
-    public Transform groundCheck; 
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    //layers, new, ground, and assign. then back in the inspector assign this to ground.   
 
     public float UpForce = 200f;
     public float moveSpeed;
-    public float jumpForce; 
+    public float jumpForce;
+    public float checkRadius; 
     private float moveDirection;
+    //to move and jump
+
+    public int maxjumpCount; 
+    private int jumpCount; 
+    //can jump however many times it wants
+
 
     private bool isJumping = false;
+    private bool isGrounded;
+
+    public Animator anim;
+
+    private AudioSource jumpSoundEffect;
+
+
+
+    //HEALTH
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    private void Start()
+    {
+        jumpCount = maxjumpCount;
+        currentHealth = maxHealth; 
+    }
+
+    //made for health to take damage when run into obstacles
+    void TakeDamage(int amount)
+    {
+        currentHealth -= amount; 
+
+        if(currentHealth <= 0)
+        {
+            //We're dead
+            //Play dead animation
+            anim.SetBool("IsDead", true); 
+        }
+
+    }
+
+    void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+    }
 
     void Awake()
     {
@@ -24,30 +77,41 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         ProcessInputs();
+        //referencing to jump
     }
     
     void FixedUpdate()
     {
+        //check if grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+        if(isGrounded)
+        {
+            jumpCount = maxjumpCount; 
+        }
+
         Move();
     }
     // Update is called once per frame
     private void Move()
     {
         moveDirection = Input.GetAxis("Horizontal");
+        
 
         rb2d.velocity = new Vector2(moveDirection * moveSpeed, rb2d.velocity.y);
         if (isJumping)
         {
             rb2d.AddForce(new Vector2(0, jumpForce));
+            jumpCount--; 
         }
         isJumping = false; 
     }
     private void ProcessInputs()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
         {
+            jumpSoundEffect.Play(); 
             isJumping = true;
         }   
     }
-    //include health, change health component, maxhealth, and playsound
+    //include health, change health component,  and playsound
 }
